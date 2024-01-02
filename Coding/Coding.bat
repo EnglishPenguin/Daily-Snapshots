2b3d7f451ac6695bad7fc101af6626cbb978f86d
David <denglish14@gmail.com>
2024-01-02 14:35:58 -0500

    Update .bat file

--- Coding/Coding.bat
+++ Coding/Coding.bat
@@ -2,11 +2,7 @@
 
 SETLOCAL
 set FILE_PATH=%~dp0
-set SCRIPT_PATH=%FILE_PATH%CM1235.py
-python -u "%SCRIPT_PATH%"
-set SCRIPT_PATH=%FILE_PATH%CM1249.py
-python -u "%SCRIPT_PATH%"
-set SCRIPT_PATH=%FILE_PATH%CM6146.py
+set SCRIPT_PATH=%FILE_PATH%coding_main.py
 python -u "%SCRIPT_PATH%"
 ENDLOCAL
 
