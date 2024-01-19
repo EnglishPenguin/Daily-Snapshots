078751c3b0aa87d902ecbc255df594f01ba5f33f
David <denglish14@gmail.com>
2024-01-18 20:31:28 -0500

    remove sleep

--- snapshot_main.py
+++ snapshot_main.py
@@ -1,12 +1,10 @@
 from snapshot import Snapshot
 from mappings import mappings_dict
 from snapshot_logger import logger
-import time
 
 if __name__ == '__main__':
     for use_case in mappings_dict:
-    # for use_case in ["MCR PartB Inactive", "Medicare Not Primary", "LIJ IS Printing", "NCOA", "BD IS Printing"]:
-        time.sleep(1)
+    # for use_case in ["MCD MCO Available"]:
         try:
             Snapshot(use_case)
         except FileNotFoundError:
