369af742ba4117ca36699a518018e83d3c1c0a6b
David <denglish14@gmail.com>
2024-02-12 08:09:07 -0500

    Ad Hoc daily snapshot

--- snapshot_main.py
+++ snapshot_main.py
@@ -4,7 +4,19 @@ from snapshot_logger import logger
 
 if __name__ == '__main__':
     for use_case in mappings_dict:
-    # for use_case in ["MCD MCO Available"]:
+    # Use the below line to run Ad Hoc daily snapshot
+        # "CSE1235"
+        # "CSE1236"
+        # "TES1249"
+        # "TES6146"
+        # "MCD MCO Available"
+        # "MCR Advantage"
+        # "MCR PartB Inactive"
+        # "Medicare Not Primary"
+        # "LIJ IS Printing"
+        # "NCOA"
+        # "BD IS Printing"
+    # for use_case in [""]:
         try:
             Snapshot(use_case)
         except FileNotFoundError:
