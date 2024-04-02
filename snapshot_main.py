890572a4ff3fa6bd450836666e8347d8bf43664a
David <denglish14@gmail.com>
2024-04-02 16:06:44 -0400

    updated to use new classes

--- snapshot_main.py
+++ snapshot_main.py
@@ -1,24 +1,49 @@
-from snapshot import Snapshot
 from mappings import mappings_dict
 from snapshot_logger import logger
+from snapshot_updated import *
+from datetime import date as dt
 
 if __name__ == '__main__':
-    for use_case in mappings_dict:
-    # Use the below line to run Ad Hoc daily snapshot
-        # "CSE1235"
-        # "CSE1236"
-        # "TES1249"
-        # "TES6146"
-        # "MCD MCO Available"
-        # "MCR Advantage"
-        # "MCR PartB Inactive"
-        # "Medicare Not Primary"
-        # "LIJ IS Printing"
-        # "NCOA"
-        # "BD IS Printing"
-    # for use_case in ["MCD MCO Available"]:
-        try:
-            Snapshot(use_case)
-        except FileNotFoundError:
-            logger.critical(f"File for {use_case} not found")
+    today = dt.today()
+    dt_func = DateFunctions()
+    date = dt_func.ask_if_correct_date(today)
+    try:
+        outbound_df = MainSpreadsheet(init_date=date)
+    except FileNotFoundError:
+        logger.critical(f"Main outbound spreadsheet not found in M:\CPP-Data\Sutherland RPA\Combined Outputs")
+    else:
+        for use_case in mappings_dict:
+            if use_case == "NCOA":
+                continue
+
+        # Use the below line to run Ad Hoc daily snapshot
+            # "CSE1235"
+            # "CSE1236"
+            # "TES1249"
+            # "TES6146"
+            # "MCD MCO Available"
+            # "MCR Advantage"
+            # "MCR PartB Inactive"
+            # "Medicare Not Primary"
+            # "LIJ IS Printing"
+            # "NCOA"
+            # "BD IS Printing"
+
+            else:
+                snapshot = Snapshot(use_case, outbound_df.main_df, date=date)
+                try:
+                    snapshot.parse_spreadsheet()
+                    if len(snapshot.use_case_df) == 0:
+                        logger.critical(f"No rows in spreadsheet for {use_case}")
+                        continue
+                    snapshot.apply_business_rules()
+                    snapshot.get_exceptions()
+                except AttributeError:
+                    logger.critical(f"{use_case} encountered an AttributeError error")
+                    continue
+                else:
+                    snapshot.calc_results()
+                    snapshot.write_email_body()
+                    snapshot.compose_and_send()
+            
         
